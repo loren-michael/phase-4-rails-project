26d628f5a72de33649ccfcfa448c405f40671f2c
loren <lorenmichaelh@gmail.com>
2023-01-09 16:05:41 -0600

    update seeds, add code to rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,18 @@
 class RentalsController < ApplicationController
+  before_action :authorize
+
+  def index
+    render json: Rental.all, only: :id, include: [:user, :movie, :store]
+  end
+
+  def show
+    find_rental
+    render json: @rental, only: :id, include: [:user, :movie, :store]
+  end
+
+  private
+
+    def find_rental
+      @rental = Rental.find_by(id: params[:id])
+    end
 end
