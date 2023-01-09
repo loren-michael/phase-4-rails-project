dd2b97fc78f4898c1d3ca7e2b31586ae1115b908
loren <lorenmichaelh@gmail.com>
2023-01-09 13:32:12 -0600

    add code to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,32 @@
 class MoviesController < ApplicationController
+
+  def index
+    render json: Movie.all
+  end
+
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+  def show
+    find_movie
+    render json: movie
+  end
+
+  def destroy
+    find_movie
+    movie.destroy
+    render json: { message: "Movie has been removed"}, status: :accepted
+  end
+
+  private
+
+    def find_movie
+      movie = Movie.find_by(id: params[:id])
+    end
+
+    def movie_params
+      params.permit(:title, :mpaa, :poster_url, :year, :runtime, :synopsis, :availability, :store_id)
+    end
 end
