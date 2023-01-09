d1b496899c0809c31c2f05e7bb653f37d0ad1c77
loren <lorenmichaelh@gmail.com>
2023-01-09 13:24:47 -0600

    add code to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_resp
+  before_action :authorize
 
+  private
+
+    def authorize
+      @current_user = User.find_by(id: session[:user_id])
+      if @current_user
+        render json: @current_user
+      else
+        render json: { errors: ["User not authorized"] }, status: :unauthorized
+      end
+    end
+
+    def render_unprocessable_entity_resp
+      render json: { errors: [exception.record.errors.full_messages] }, status: :unprocessable_entity
+    end
 end
